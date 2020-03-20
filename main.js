b1bd8a5d6d1b6601aa8fa435b33be6c08019214f
milan-stefanik <stefanik.milan@gmail.com>
2020-03-20 12:21:36 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
-function getData(cb) {
+const baseURL = "https://swapi.co/api/";
 
-var xhr = new  XMLHttpRequest();
+function getData(type, cb) {
 
-xhr.open("GET", "https://swapi.co/api/");
-xhr.send();
+    var xhr = new  XMLHttpRequest();
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        //setData(JSON.parse(this.responseText));
-        //data=this.responseText;
-        //console.log(data);
-        cb(console.log(JSON.parse(this.responseText)));
-        //console.log(typeof(JSON.parse(this.responseText)));
-        //document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
 }
 
-getData(function(data) {
-    console.log(data)
-});
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+           var dataRow = [];            
+           Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15); 
+                dataRow.push(`<td>${truncatedData}</td>`);
+           });
+           tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
